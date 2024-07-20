ab41cdbe53d52e7f49092454b8ead72291676b5e
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-20 15:11:38 -0700

    refactor: move NextEventPlayer to its own file

--- test/domain/entities/next_player_test.dart
+++ test/domain/entities/next_player_test.dart
@@ -1,32 +1,5 @@
+import 'package:clean_arch_soccer_flutter/domain/entities/next_event_player.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-class NextEventPlayer {
-  final String id;
-  final String name;
-  late final String initials;
-  final String? photo;
-  final String? position;
-  final bool isConfirmed;
-  final DateTime? confirmationDate;
-
-  NextEventPlayer(
-      {required this.id,
-      required this.name,
-      required this.isConfirmed,
-      this.photo,
-      this.position,
-      this.confirmationDate
-      }) {
-      initials = _getInitials();
-    }
-
-  String _getInitials() {
-    final names = name.toUpperCase().trim().split(' ');
-    final firstChar = names.first.split('').firstOrNull ?? '-';
-    final lastChar = names.last.split('').elementAtOrNull(names.length == 1 ? 1 : 0) ?? '';
-    return '$firstChar$lastChar';
-  }
-}
 
 void main() {
   String initialsOf(String name) =>
